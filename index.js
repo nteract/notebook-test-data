a016d8f9d784757b07ae48aa13e53e698745aba6
Safia Abdalla <safia@safia.rocks>
2016-07-27 10:29:47 -0600

    Seperate 4.0 and 4.1 notebooks

--- index.js
+++ index.js
@@ -6,15 +6,24 @@ function p(nb) {
 
 module.exports = {
   valid: {
-    v4: {
-      multiples: p("multiples.ipynb"),
-      ansi: p("ANSITest.ipynb"),
-      css: p("CSSReference.ipynb"),
-      confinedOutput: p("ConfinedOutput.ipynb"),
-      functional: p("Functional.ipynb"),
-      outputCallbacks: p("TestOutputCallbacks.ipynb"),
-      v4notebook: p("test4.ipynb"),
-      v4notebookplus: p("test4plus.ipynb")
+    v4.0: {
+      multiples: p("4.0/multiples.ipynb"),
+      ansi: p("4.0/ANSITest.ipynb"),
+      css: p("4.0/CSSReference.ipynb"),
+      confinedOutput: p("4.0/ConfinedOutput.ipynb"),
+      functional: p("4.0/Functional.ipynb"),
+      outputCallbacks: p("4.0/TestOutputCallbacks.ipynb"),
+      v4notebook: p("4.0/test4.ipynb"),
+      v4notebookplus: p("4.0/test4plus.ipynb")
+    },
+    v4.1: {
+      multiples: p("4.1/multiples.ipynb"),
+      ansi: p("4.1/ANSITest.ipynb"),
+      css: p("4.1/CSSReference.ipynb"),
+      confinedOutput: p("4.1/ConfinedOutput.ipynb"),
+      functional: p("4.1/Functional.ipynb"),
+      outputCallbacks: p("4.1/TestOutputCallbacks.ipynb"),
+      v4notebook: p("4.1/test4.ipynb"),
     },
     v3: {
       v3notebook: p("test3.ipynb")
@@ -24,6 +33,6 @@ module.exports = {
     }
   },
   invalid: {
-    v4: p("invalid.ipynb"),
+    v4: p("4.0/invalid.ipynb"),
   },
 }
